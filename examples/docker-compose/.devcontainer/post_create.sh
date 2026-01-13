f3e31844bf392cfa83eeeb6b2b97ecb64ad4ec0b
Sondre Arneberg <sondre.arneberg@bidbax.no>
2026-01-13 14:06:53 +0100

    fix: undo flow-map-viz post_create action

--- examples/docker-compose/.devcontainer/post_create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# add bash aliases to .bash_aliases
-cat .devcontainer/aliases.zsh >> ~/.oh-my-zsh/custom/aliases.zsh
-
-# extract flow-map-viz app to sh1s apps
-tar -xzf /workspace/flow-map-viz_1515.tgz -C /opt/splunk/etc/apps
